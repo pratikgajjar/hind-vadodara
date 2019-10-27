3039cf216593e09e384a060caefcd6a43d22b82b
Pratik Gajjar <pmgajjar25@gmail.com>
2019-10-27 16:52:54 +0530

    Remove py2.7

--- build.py
+++ build.py
@@ -2,45 +2,25 @@
 
 import hindkit as kit
 
-# - - -
+def main():
+    family = kit.Family(
+        trademark='Hind Vadodara',
+        script_name='Gujarati',
+    )
+    family.set_masters()
+    family.set_styles()
+    builder = kit.Project(
+        family,
+        options={
+            "do_style_linking": True,
+            "additional_unicode_range_bits": [1, 2],
+            "use_os_2_version_4": True,
+            "prefer_typo_metrics": True,
+            "build_ttf": True,
+        },
+    )
+    builder.build()
 
-family = kit.Family(
-    trademark = 'Hind Vadodara',
-    script_name = 'Gujarati',
-)
-#
-# family.set_masters(
-#     [
-#         ("100", -50),
-#         ("500", 44),
-#         ("900", 150),
-#     ]
-# )
-family.set_masters()
-family.set_styles()
 
-# - - -
-
-builder = kit.Project(family)
-builder.build()
-#
-# builder.set_options([
-#
-#     'prepare_styles',   # stage i
-#     'prepare_features', # stage ii
-#     'compile',          # stage iii
-#
-#     'makeinstances', #!
-#     'checkoutlines', #!
-#     # 'autohint',      #!
-#
-#     'do_style_linking',
-#     'use_os_2_version_4',
-#     'prefer_typo_metrics',
-#     'is_width_weight_slope_only',
-#
-# ])
-
-# builder.generate_designspace()
-# builder.generate_fmndb()
-
+if __name__ == '__main__':
+    main()
